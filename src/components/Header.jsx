ab0f579f86adf884924865f4d4fccb3dd1de70ba
Yousef Na <yousefnasr0541@gmail.com>
2023-10-23 22:40:37 +0300

    use pseudo selector & media Queries

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.png";
 
 export default function Header() {
 	return (
-		<header className=" flex flex-col items-center mt-8 mb-16">
+		<header className=" flex flex-col items-center mt-8 mb-8 md:mb-16">
 			<img
 				className=" mb-8 w-44 h-44 object-contain"
 				src={logo}
@@ -11,7 +11,7 @@ export default function Header() {
 			<h1 className="text-xl font-semibold tracking-widest text-center uppercase md:text-4xl text-amber-800 font-title">
 				ReactArt
 			</h1>
-			<p className="text-stone-500">A community of artists and art-lovers.</p>
+			<p className="text-stone-500 ">A community of artists and art-lovers.</p>
 		</header>
 	);
 }
